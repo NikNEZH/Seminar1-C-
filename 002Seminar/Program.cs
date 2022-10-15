5df04e384c28448818410e2914140c6ac265370f
Nikita <kit.zaver@yandex.ru>
2022-10-15 10:47:37 +0300

    ADD 002

---
+++ 002Seminar/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Ввудите число чтобы получить его в квадрате");
+string numberAStr = Console.ReadLine() ?? ""; 
+string numberBStr = Console.ReadLine() ?? ""; 
+int numberA = int.Parse(numberAStr); 
+int numberB = int.Parse(numberBStr);
+int kvadrat = numberA * numberA; 
+if (kvadrat == numberB)
+{
+    Console.WriteLine($"Результат числа {numberA} равен { numberB}");
+    
+} else
+{
+    Console.WriteLine($"Результат числа {numberA} не равен { numberB}");
+}
+
