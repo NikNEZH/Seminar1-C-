e4b334a47dbb653a176baa1ffee88e2952ebd8b9
Nikita <kit.zaver@yandex.ru>
2022-10-15 10:34:47 +0300

    ADD 001

---
+++ 001Seminar/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Ввудите число чтобы получить его в квадрате");
+string numberStr = Console.ReadLine() ?? ""; 
+int number = int.Parse(numberStr); 
+int result = number * number; 
+Console.WriteLine($"Результат числа {number} равен { result}");
